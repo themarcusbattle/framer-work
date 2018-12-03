f70c612b7620d3f0c64f155c27763cc0a1ba6070
Marcus Battle <themarcusbattle@gmail.com>
2018-12-03 03:59:55 -0500

    Made site responsive

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title><?php echo get_bloginfo(); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php wp_head(); ?>
     </head>
     <body>
