dc8718d8630852c16b5ee61dd1ead1db98676dd8
Marcus Battle <themarcusbattle@gmail.com>
2018-12-03 04:03:17 -0500

    Help Google stop translating the site

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
     <head>
         <title><?php echo get_bloginfo(); ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Language" content="en">
         <?php wp_head(); ?>
     </head>
     <body>
